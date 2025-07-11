9c74671bbd865acb70d8b80d69e9eff1a96930b5
Vedant Navle <vedantnavale02@gmail.com>
2025-07-11 16:55:08 +0530

    Update application.py

--- application.py
+++ application.py
@@ -39,4 +39,4 @@ def predict_datapoint():
     else:
         return render_template('home.html',results_score='please enter the data first')
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
